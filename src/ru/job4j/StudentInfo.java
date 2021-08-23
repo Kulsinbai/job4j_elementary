c8a492ff1ffd9dfde3d221fc0674f1fd9c12cad5
Kulsinbai <mcc.kulsinbai@gmail.com>
2021-08-23 06:55:24 +0500

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ilyas Kulsinbaev");
+        System.out.println("20.05.1994");
+    }
+}
