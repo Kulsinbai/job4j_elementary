853341c3a93e72cfb34784ccab57bdcef23ed632
Kulsinbai <mcc.kulsinbai@gmail.com>
2022-01-17 20:57:26 +0500

    6.0.1. Объявление массива. [#3613 #252100]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,14 +2,8 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        String[] names = new String[4];
-        names[0] = "Benzema";
-        names[1] = "Kimmich";
-        names[2] = "Van Dijk";
-        names[3] = "Neuer";
-        System.out.println("Лучший нападающий: " + names[0]);
-        System.out.println("Лучший полузащитник: " + names[1]);
-        System.out.println("Лучший защитник: " + names[2]);
-        System.out.println("Лучший вратарь: " + names[3]);
+        short [] ages = new short[10];
+        String [] surnames = new String[100500];
+        float [] prices = new float[40];
     }
 }
