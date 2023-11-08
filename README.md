75fa123023a28045b2882046a8f01e98144f86cf
Kulsinbai <mcc.kulsinbai@gmail.com>
2023-11-08 23:04:04 +0500

    1

--- README.md
+++
@@ -1,6 +0,0 @@
-# job4j
-Проект содержит решения блока "Базовый синтаксис" курса Job4j.
-
-![Image of Yaktocat](https://github.com/Kulsinbai/job4j_elementary/blob/master/images/IDEA%201.png)
-
-Электронная почта для связи со мной kulsinbai@mail.com
--- src/main/java/ru/job4j/condition/Types.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.condition;
-
-public class Types {
-    public static void main(String[] args) {
-        long l = 129;
-        byte b = (byte) l;
-        System.out.println("Вывод в консоль значения после расширения: " + l);
-    }
-}
