49d2d4e6fcb82590a42008fe31159d4b31bc3ce2
Kulsinbai <mcc.kulsinbai@gmail.com>
2024-04-25 23:45:29 +0500

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + ages.length);
-        System.out.println("Размер массива равен: " + surnames.length);
-        System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Benzema";
+        names[1] = "Kimmich";
+        names[2] = "Van Dijk";
+        names[3] = "Neuer";
+        System.out.println("Лучший нападающий: " + names[0]);
+        System.out.println("Лучший полузащитник: " + names[1]);
+        System.out.println("Лучший защитник: " + names[2]);
+        System.out.println("Лучший вратарь: " + names[3]);
     }
 }
